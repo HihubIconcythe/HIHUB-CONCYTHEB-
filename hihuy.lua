7f5e887d64ece42c3a0b10a2b45eced26d15882c
HihubIconcythe <157896826+HihubIconcythe@users.noreply.github.com>
2024-01-28 11:57:57 +0700

    Add files via upload

---
+++ hihuy.lua
@@ -0,0 +1,251 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+local Window = OrionLib:MakeWindow({HIHUB|CONCYTHE = "YNZ Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "YNKfolder"})
+
+local Tab = Window:MakeTab({
+
+	HIHUB|CONCYTHE = "scripts",	Icon = "rbxassetid://14009156449",
+
+	PremiumOnly = false
+
+})
+
+local Tab2 = Window:MakeTab({
+
+	HIHUB|CONCYTHE = "blox fruit scripts",
+
+	Icon = "rbxassetid://10660406374",
+
+	PremiumOnly = false
+
+})
+
+local Tab3 = Window:MakeTab({
+
+	HIHUB|CONCYTHE = "random blox fruit",
+
+	Icon = "rbxassetid://10660406374",
+
+	PremiumOnly = false
+
+})
+
+Tab2:AddButton({
+
+	HIHUB|CONCYTHE = "domadic hub",
+
+	Callback = function()
+
+      		print("DOMAH")
+
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Domadicoof/Domadicoof/main/Domadichub/NottoGay/Start.ranscript"))()
+
+  	end    
+
+})
+
+Tab:AddButton({
+
+	HIHUB|CONCYTHE = "Remote spy mobile ",
+
+	Callback = function()
+
+	      		print("remoted")
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/RS/main/SimpleSpyMobile"))()
+
+  	end    
+
+})
+
+Tab2:AddButton({
+
+	HIHUB|CONCYTHE = "BLCK HUB",
+
+	Callback = function()
+
+	      		print("BLCKH")
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/1f0yt/community/main/BLCK"))()
+
+  	end    
+
+})
+
+Tab:AddButton({
+
+	HIHUB|CONCYTHE = "infinity yield",
+
+	Callback = function()
+
+	      		print("Infinity")
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+
+  	end    
+
+})
+
+Tab3:AddButton({
+
+	HIHUB|CONCYTHE = "redeem codes",
+
+	Callback = function()
+
+	      		print("Codes Blox Fruit")
+
+local args = {
+
+    [1] = "Sub2CaptainMaui"
+
+}
+
+game:GetService("ReplicatedStorage").Remotes.Redeem:InvokeServer(unpack(args))
+
+print("code 1")
+
+local args = {
+
+    [1] = "kittgaming"
+
+}
+
+game:GetService("ReplicatedStorage").Remotes.Redeem:InvokeServer(unpack(args))
+
+print("code 2")
+
+local args = {
+
+    [1] = "SUB2DAIGROCK"
+
+}
+
+game:GetService("ReplicatedStorage").Remotes.Redeem:InvokeServer(unpack(args))
+
+print("code 3")
+
+local args = {
+
+    [1] = "Sub2Fer999"
+
+}
+
+game:GetService("ReplicatedStorage").Remotes.Redeem:InvokeServer(unpack(args))
+
+print("code 4")
+
+local args = {
+
+    [1] = "Enyu_is_Pro"
+
+}
+
+game:GetService("ReplicatedStorage").Remotes.Redeem:InvokeServer(unpack(args))
+
+print("code 5")
+
+local args = {
+
+    [1] = "Magicbus"
+
+}
+
+game:GetService("ReplicatedStorage").Remotes.Redeem:InvokeServer(unpack(args))
+
+print("code 6")
+
+local args = {
+
+    [1] = "JCWK"
+
+}
+
+game:GetService("ReplicatedStorage").Remotes.Redeem:InvokeServer(unpack(args))
+
+print("code 7")
+
+local args = {
+
+    [1] = "TantaiGaming"
+
+}
+
+game:GetService("ReplicatedStorage").Remotes.Redeem:InvokeServer(unpack(args))
+
+print("code 8")
+
+local args = {
+
+    [1] = "Axiore"
+
+}
+
+game:GetService("ReplicatedStorage").Remotes.Redeem:InvokeServer(unpack(args))
+
+print("code 9")
+
+local args = {
+
+    [1] = "SUB2NOOBMASTER123"
+
+}
+
+game:GetService("ReplicatedStorage").Remotes.Redeem:InvokeServer(unpack(args))
+
+print("code 10")
+
+local args = {
+
+    [1] = "Sub2UncleKizaru"
+
+}
+
+game:GetService("ReplicatedStorage").Remotes.Redeem:InvokeServer(unpack(args))
+
+print("code 11")
+
+local args = {
+
+    [1] = "SUB2GAMERROBOT_RESET1"
+
+}
+
+game:GetService("ReplicatedStorage").Remotes.Redeem:InvokeServer(unpack(args))
+
+print("code 12")
+
+local args = {
+
+    [1] = "Thegreatace"
+
+}
+
+game:GetService("ReplicatedStorage").Remotes.Redeem:InvokeServer(unpack(args))
+
+print("code 13")
+
+local args = {
+
+    [1] = "SUB2GAMERROBOT_EXP1"
+
+}
+
+game:GetService("ReplicatedStorage").Remotes.Redeem:InvokeServer(unpack(args))
+
+print("code 14")
+
+local args = {
+
+    [1] = "shipbattle"
+
+}
+
+game:GetService("ReplicatedStorage").Remotes.Redeem:InvokeServer(unpack(args))
+
+print("code 15 zioles code")
+
+  	end    
+
+})
